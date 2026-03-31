f4558ca35c79401a04c22974ec3bf41c6fdf2542
Filipe <filipesilveira.dev@gmail.com>
2026-03-31 16:02:01 -0300

    Alteração de segurança em dependência
    Alteração na dependência axios (versão 1.13.6), removendo o "^" antes da versão, prevenindo a atualização automática de axios após "npm install". A versão 1.14.1 está comprometida e infectada com malware.

--- tudo-dev-blog-nextjs/src/components/Grid/index.tsx
+++ tudo-dev-blog-nextjs/src/components/Grid/index.tsx
@@ -1,13 +1,17 @@
+// Importação da interface “Article” criada no local especificado
 import type {Article} from "@/types/types"
+// Importação do componente <Card /> que será renderizado dentro de um map() feito no array “articles” passado via props de Home()
 import Card from "../Card"
+// Importação da estilização
 import styles from "./Grid.module.css"
 
+// Tipagem typescript: é esperado um array “articles” tipado de acordo com a interface criada “Article”
 type Props ={
     articles: Article[]
 }
 
+// Recebe o objeto “articles” via props de Home(): ele será utilizado para criar um card para cada artigo salvo
 const Grid = ({articles} : Props)=>{
-    const artigo = articles.find((article) => article.id == "1")
     return(
         // Local onde aparecerão os filmes. Serão renderizados tantos componentes "Card", que representará cada filme, quanto houver na lista (array) de filmes
         <section className={styles.grid}>
