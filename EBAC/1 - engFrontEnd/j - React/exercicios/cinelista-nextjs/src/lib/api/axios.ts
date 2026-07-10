d8ae78a5e68fd331251fa2b1cf15cc0c413d8694
Filipe <filipesilveira.dev@gmail.com>
2026-07-10 16:34:02 -0300

    atualização axios

--- EBAC/1 - engFrontEnd/j - React/exercicios/cinelista-nextjs/src/lib/api/axios.ts
+++ EBAC/1 - engFrontEnd/j - React/exercicios/cinelista-nextjs/src/lib/api/axios.ts
@@ -1,13 +1,13 @@
-// Arquivo criado para receber uma instância de axios com configurações padrão para realizar uma requisição. 
-// Dessa forma, não será necessário a cada requisição repetir isso.
+// Arquivo criado para receber uma instância de axios com configurações padrão para realizar uma requisição. Dessa forma, não será necessário a cada requisição repetir isso
 import axios from "axios";
 
 // Perceba que são os dados necessários nas requisições. Para realizar um GET, por exemplo, basta utilizar tmdbApi.get("/api"), ao invés de escrever tudo toda vez.
 const tmdbApi = axios.create({
-  // Se process.env.TMDB_API_URL estiver indefinido na Vercel, o Axios usará a string padrão abaixo:
+  // o "process.env" serve para utilizar a variável criada localmente sem expor no github
   baseURL: process.env.TMDB_API_URL || "https://api.themoviedb.org/3",
   headers: {
-    // Caso a variável de ambiente falhe, envia uma string vazia para evitar erros de concatenação
+    // ** Dado sensível **
+    // Caso seja escrito aqui, esse dado ficará exposto.
     Authorization: `Bearer ${process.env.TMDB_API_KEY || ""}`,
     "Content-type": "application/json",
   },
