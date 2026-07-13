8903bd5dde60371ca5f69b1813ab63b95f6b17e7
Filipe <filipesilveira.dev@gmail.com>
2026-07-13 20:49:59 -0300

    mais detalhes de mensagem

--- exemplo_devops_ebac/app/components/Message.tsx
+++ exemplo_devops_ebac/app/components/Message.tsx
@@ -4,13 +4,13 @@ interface MessageProps {
 
 export default function Message({ textColorClass }: MessageProps) {
   return (
-    <div className="flex flex-col justify-between items-center gap-2">
+    <div className="flex flex-col justify-between items-center gap-4">
       <h1 className={`font-bold text-2xl ${textColorClass}`}>Exemplo DevOps</h1>
       <p className={`text-center ${textColorClass}`}>
         Exercício criado para exemplificar o funcionamento de um Pipeline CI/CD.
         O projeto contém:
       </p>
-      <ul className={`text-center ${textColorClass}`}>
+      <ul className={`flex text-center ${textColorClass}`}>
         <li>
           - <strong>Custom Hook:</strong> contém uma função que altera
           estilização de elementos HTML
@@ -19,7 +19,7 @@ export default function Message({ textColorClass }: MessageProps) {
           - <strong>Teste do hook:</strong> teste realizado para verificar o
           funcionamento do custom hook
         </li>
-        <li></li>
+        <li>- <strong>Pipeline CI/CD</strong> com validação de código, build da aplicação e deploy automatizado</li>
       </ul>
     </div>
   );
