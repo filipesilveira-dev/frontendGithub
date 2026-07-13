d4eba2f0764ca88ba1311a62520f9251092e68e9
Filipe <filipesilveira.dev@gmail.com>
2026-07-13 18:04:35 -0300

    documentação no arquivo de teste

--- exemplo_devops_ebac/page.test.tsx
+++ exemplo_devops_ebac/page.test.tsx
@@ -1,14 +1,21 @@
-import {jest} from '@jest/globals'
-import { renderHook, act } from '@testing-library/react';
-import { useStyleSwitcher, colors } from './app/hook/useStyleSwitcher'; // Importe o hook e o array
+// Possibilita criar mocks
+import { jest } from "@jest/globals";
+// Hooks não podem ser chamados como funções comuns (renderHook)
+import { renderHook, act } from "@testing-library/react";
+// Importa o hook a ser testado e a lista de cores que utiliza
+import { useStyleSwitcher, colors } from "./app/hook/useStyleSwitcher"; // Importe o hook e o array
 
-describe('useStyleSwitcher', () => {
-  it('deve alternar para uma cor diferente da atual', () => {
-    // 1. Arrange: Mock do Math.random
-    // O primeiro sorteio retorna 0 (índice 0, cor atual: 'text-black')
-    // O segundo sorteio retorna 0.99 (índice 3, cor: 'text-blue-500')
-    const randomSpy = jest.spyOn(Math, 'random')
-      .mockReturnValueOnce(0)      
+// Agrupa os testes (todos se referem ao hook useStyleSwitcher)
+describe("useStyleSwitcher", () => {
+    // Descreve o comportamento esperado
+  it("deve alternar para uma cor diferente da atual", () => {
+
+    // 1. Arrange: Mock do Math.random: o "spyOn" permite controlar o resultado randômico
+    const randomSpy = jest
+      .spyOn(Math, "random")
+      // O primeiro sorteio retorna 0 (índice 0, cor atual: 'text-black')
+      .mockReturnValueOnce(0)
+      // O segundo sorteio retorna 0.99 (índice 3, cor: 'text-blue-500')
       .mockReturnValueOnce(0.99);
 
     // Passamos a constante 'colors' para o hook
@@ -20,16 +27,16 @@ describe('useStyleSwitcher', () => {
     });
 
     // 3. Assert: Verifica se o estado mudou para a cor do índice 3
-    expect(result.current.currentStyle.text).toBe('text-blue-500');
+    expect(result.current.currentStyle.text).toBe("text-blue-500");
     expect(randomSpy).toHaveBeenCalledTimes(2);
 
     // Limpeza
     randomSpy.mockRestore();
   });
 
-  it('deve inicializar com a primeira cor do array fornecido', () => {
+  it("deve inicializar com a primeira cor do array fornecido", () => {
     const { result } = renderHook(() => useStyleSwitcher(colors));
-    
+
     expect(result.current.currentStyle).toEqual(colors[0]);
   });
 });
