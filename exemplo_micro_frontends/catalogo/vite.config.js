33bbe390402433e40bbf591a5088fa594aafb8b1
Filipe <filipesilveira.dev@gmail.com>
2026-07-30 11:18:08 -0300

    porta de catalogo adicionada. Repetir o processo com carrinho

--- exemplo_micro_frontends/catalogo/vite.config.js
+++ exemplo_micro_frontends/catalogo/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 //Importação do plugin de federação
-import { federation } from '@module-federation/vite' 
+import { federation } from "@module-federation/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,24 +12,31 @@ export default defineConfig({
     //Configuração do Module Federation
     federation({
       // o container usará esse nome
-      name: 'catalogo', 
+      name: "catalogo",
       // nome do arquivo que será gerado no build
-      filename: 'remoteEntry.js', 
+      filename: "remoteEntry.js",
       exposes: {
         // expondo o componente "Catalogo"para o container
-         './Catalogo': './src/components/Catalogo', 
+        "./Catalogo": "./src/components/Catalogo",
       },
       shared: {
         // compartilhamento de dependências para evitar conflitos de versão
         react: { singleton: true },
-        'react-dom': { singleton: true }
+        "react-dom": { singleton: true },
       },
       // Desativa a busca por configurações do TypeScript e a geração de tipos
-      dts: false
-    })
+      dts: false,
+    }),
   ],
+  // definição da porta que será utilizada no servidor local
+  server: {
+    // define a porta fixa
+    port: 3001,
+    // se a 3001 estiver ocupada, ele falha em vez de mudar para 3002
+    strictPort: true,
+  },
   // Configuração de build necessária para habilitar o Top-Level Await do Module Federation. O Vite utiliza o ESbuild originalmente
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-})
+});
