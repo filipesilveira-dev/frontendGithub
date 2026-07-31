24ce5d8013675deb39a0787c0d4058aee283bae9
Filipe <filipesilveira.dev@gmail.com>
2026-07-31 11:14:27 -0300

    adição do botão que dispara o evento 'addToCheckout' ao Card.

--- sepia_bistro_micro_frontend/mfe-menu/src/components/Card.jsx
+++ sepia_bistro_micro_frontend/mfe-menu/src/components/Card.jsx
@@ -1,21 +1,29 @@
-const Card = ({menuItems})=>{
-    
-    return(
-        <ul>
-            {
-                menuItems.map((item)=>(
-                    <li key={item.id}>
-                        <div>
-                            <img src="https://placehold.co/300x200" alt="" />
-                            <h3>{item.name}</h3>
-                            <p>{item.description}</p>
-                            <p>{item.price}</p>
-                        </div>
-                    </li>
-                ))
-            }
-        </ul>
-    )
-}
+const Card = ({ menuItems }) => {
+  return (
+    <ul>
+      {menuItems.map((item) => (
+        <li key={item.id}>
+          <div>
+            <img src="https://placehold.co/300x200" alt="" />
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </div>
+          <button
+            onClick={() => {
+              // dispara (emite) esse evento globalmente para o objeto "window" do navegador. Qualquer parte da aplicação que esteja "escutando" esse evento específico (geralmente usando "window.addEventListener('adicionarCarrinho', callback)") será notificada imediatamente
+              window.dispatchEvent(
+                // cria um novo evento customizado no navegador chamado "adicionarCarrinho". É por meio do "detail" que você envia dados junto com o evento (a variável "p" nesse caso)
+                new CustomEvent("addToCheckout", { detail: item }),
+              );
+            }}
+          >
+            Adicionar
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default Card
+export default Card;
